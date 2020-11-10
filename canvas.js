f5cc93c6153b864ea675b6fcff24e3606cdb3521
Zenek <35767830+Zenek-Hastro@users.noreply.github.com>
2020-11-10 14:31:36 +0100

    try to fix :/

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById('canvas-js'),
+var canvas = document.getElementById('nokey'),
    can_w = parseInt(canvas.getAttribute('width')),
    can_h = parseInt(canvas.getAttribute('height')),
    ctx = canvas.getContext('2d');
