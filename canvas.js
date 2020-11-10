12cd866dad54b5a1d3a1f5fddc5c3cc163ed32ed
Zenek <35767830+Zenek-Hastro@users.noreply.github.com>
2020-11-10 14:15:17 +0100

    try to fix the background

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById('nokey'),
+var canvas = document.getElementById('canvas'),
    can_w = parseInt(canvas.getAttribute('width')),
    can_h = parseInt(canvas.getAttribute('height')),
    ctx = canvas.getContext('2d');
